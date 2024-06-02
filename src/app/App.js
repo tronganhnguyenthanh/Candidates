eb8e48463c3957962d44ea31f3ddf1e1379ad3e5
Phuong Nguyen <tronganhnguyen222@gmail.com>
2024-06-02 17:14:00 +0700

    [UPDATE][FE] restore code

--- src/app/App.js
+++ src/app/App.js
@@ -3,12 +3,10 @@ import FormAdd from "../components/FormAdd"
 import CandidateDetail from "../components/CandidateDetail"
 import FormEdit from "../components/FormEdit"
 import CandidateList from "../components/CandidateList"
-import ScanQR from "../components/scanQrCode/ScanQR"
 const App = () => {
   return (
    <div className="App">
      <Routes>
-       <Route path="/scan/qrCode" element={<ScanQR/>}/>
        <Route path="/" element={<FormAdd/>}/>
        <Route path="/candidates/list" element={<CandidateList/>}/>
        <Route path="/candidate/edit/:objectId" element={<FormEdit/>}/>
--- src/components/scanQrCode/ScanQR.js
+++ src/components/scanQrCode/ScanQR.js
@@ -3,7 +3,10 @@ import QRCode from "react-qr-code"
 const ScanQR = () => {
  const url = "https://candidates-beta.vercel.app/"
  return(
-  <div style={{height:"auto", margin:"0 auto", maxWidth:257, width:"100%", marginTop:150}}>
+  <div 
+    style={{height:"auto", margin:"0 auto", maxWidth:257, width:"100%", marginTop:150}} 
+    className="responsive"
+   >
     <QRCode
       size={256}
       style={{height:"auto", maxWidth:"100%", width:"100%"}}
